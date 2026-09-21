be211f828f888b1765a083c7da21504bf229f658
gurjung <gurjung997@gmail.com>
2026-09-21 20:32:40 +0530

    feat(routes): set up react-router and add base route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Navbar";
+import Body from "./components/body/Body";
+import Feed from "./components/feed/Feed";
+import Login from "./components/auth/login/Login";
+import Profile from "./components/profile/Profile";
+import Connections from "./components/connections/Connections";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <Navbar />
-    </div>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/connections" element={<Connections />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
