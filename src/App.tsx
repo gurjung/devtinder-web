1adc6e905c951cd2b67b18ce2b0828f776ef0c5d
gurjung <gurjung997@gmail.com>
2026-09-21 19:40:45 +0530

    chore: setup Vite React project with Tailwind CSS and Navbar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
